4dacd533947aa14b5c2b4931eda7ff6a13198832
newton <newtonnarzary28@gmail.com>
2025-09-08 07:37:01 +0400

    [ADD] added shadcn + custom shadcn styles + theme switching

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import { SignInButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
+import { ThemeToggle } from "@/components/ThemeToggle";
 
 export default function Home() {
   return (
-    <>
-      <SignInButton />
-      <UserButton />
-    </>
+    <div className="flex flex-col">
+      <div className="flex justify-end p-4">
+        <ThemeToggle />
+      </div>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <SignInButton />
+        <UserButton />
+      </div>
+    </div>
   );
 }
